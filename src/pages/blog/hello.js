0c0414c372268109e5109801f6ded73ff666420d
John McDonald <jmcdonald1991@gmail.com>
2021-02-11 15:45:10 -0600

    material ui

--- src/pages/blog/hello.js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Post() {
-  const router = useRouter();
-  const { slug } = router.query;
-
-  return <h1>Hello! Post Slug: {slug}</h1>;
-}
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/Layout";
-
-const Index = ({ currentUser }) => {
-  return <Layout>{currentUser ? currentUser : "Please Sign In."}</Layout>;
-};
-
-export default Index;
